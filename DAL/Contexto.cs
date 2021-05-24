3d7ff3ccab300a931e1b5f0cc488a8fbaa948c35
Raldy Steven Lopez Gilbert <raldy_lopez@ucne.edu.do>
2021-05-23 22:44:34 -0400

    Agregar archivos de proyecto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrodePrestamo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrodePrestamo.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options): base(options)
+        {
+            
+        }
+
+
+
+    }
+}
+
+
+  
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RegistrodePrestamos.Data;
+using Blazored.Toast;
+using RegistrodePrestamos.BLL;
+using RegistrodePrestamo.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistrodePrestamos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddBlazoredToast();
+            services.AddSingleton<WeatherForecastService>();
+
+            services.AddDbContext<Contexto>(options =>
+            {
+                options.UseSqlite(@"Data Source = Data\\Prestamos.db");
+            });
+
+            services.AddScoped<PersonasBLL>();
+            services.AddScoped<PrestamosBLL>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
